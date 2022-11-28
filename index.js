f55330442456887b8099acdd6cd19d34c243c043
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-11-28 14:27:14 +0300

    comments

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+//A grading sysytem to determine grades for students ranging from 0-100 assigning A,B,C,D or E accordingly
+
 function showValue(value){
+    //declaring values
     let grade;
+
+    //if statements
 
     if(value > 79){
         grade = 'You got an A';
@@ -19,14 +24,18 @@ function showValue(value){
     console.log (grade)
 return grade
 }
+//testing if the code is working
 showValue(50)
 showValue(26)
 showValue(88)
 showValue(2)
 
+//award demerit points according to the exceeded speed over 70 drom 0 to 12 demerits
 
 function licenseValidity(speed){
+    //declaring function
     let demerit;
+    //if statements
     if (speed<=70){
         demerit="okay";
     }else if(((speed-70)/5)>=12){
@@ -36,6 +45,7 @@ function licenseValidity(speed){
     }
     console.log(demerit)
 }
+//testing if the code is working
 licenseValidity(220)
      licenseValidity(90)
      licenseValidity(69)
